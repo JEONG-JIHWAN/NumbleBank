7f3154cad264fe830b8c0e273654e06e3c996301
JEONG-JIWHAN <jjw981210@gmail.com>
2023-02-12 16:18:19 +0900

    feat: project settings

---
+++ src/main/java/com/bankingserver/numblebank/NumblebankApplication.java
@@ -0,0 +1,13 @@
+package com.bankingserver.numblebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumblebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumblebankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankingserver/numblebank/NumblebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingserver.numblebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumblebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
