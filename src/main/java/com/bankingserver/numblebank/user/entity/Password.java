abeebd6ce00bd41d4be03704dcf0d770299730a2
JEONG-JIWHAN <jjw981210@gmail.com>
2023-02-17 14:25:40 +0900

    feat: Password VO 생성

---
+++ src/main/java/com/bankingserver/numblebank/user/entity/Password.java
@@ -0,0 +1,33 @@
+package com.bankingserver.numblebank.user.entity;
+
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class Password {
+    private final String value;
+
+    public static Password valueOf(String value) {
+        return new Password(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Password password = (Password) o;
+        return Objects.equals(value, password.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
