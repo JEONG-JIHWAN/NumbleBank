6f6fbc95188c156d9737df00c21a0725e017fd87
JEONG-JIWHAN <jjw981210@gmail.com>
2023-02-17 14:25:54 +0900

    feat: UserId VO 생성

---
+++ src/main/java/com/bankingserver/numblebank/user/entity/UserId.java
@@ -0,0 +1,33 @@
+package com.bankingserver.numblebank.user.entity;
+
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserId {
+    private final String value;
+
+    public static UserId valueOf(String value) {
+        return new UserId(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserId userId = (UserId) o;
+        return Objects.equals(value, userId.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
