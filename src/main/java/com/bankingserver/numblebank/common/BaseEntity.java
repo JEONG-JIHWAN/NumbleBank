efc64b572e9469eb2499ca70011bfd0f8c7b2c09
JEONG-JIWHAN <jjw981210@gmail.com>
2023-02-17 14:05:23 +0900

    feat: BaseEntity 생성

---
+++ src/main/java/com/bankingserver/numblebank/common/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.bankingserver.numblebank.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(columnDefinition = "TIMESTAMP", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
