de80cbdd87f8c671ff8b43744cc9dfe52ec259b7
JEONG-JIWHAN <jjw981210@gmail.com>
2023-02-18 03:32:27 +0900

    feat: SecurityConfig 설정

---
+++ src/main/java/com/bankingserver/numblebank/common/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.bankingserver.numblebank.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                        .requestMatchers("/sign-up").permitAll()
+                        .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .rememberMe().disable();
+        return http.build();
+    }
+}
