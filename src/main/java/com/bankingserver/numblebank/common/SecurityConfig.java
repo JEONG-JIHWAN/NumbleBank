59c258e05f58f005df15bed404c57725de215037
JEONG-JIWHAN <jjw981210@gmail.com>
2023-02-20 17:27:18 +0900

    feat: SecurityConfig 설정

--- src/main/java/com/bankingserver/numblebank/common/SecurityConfig.java
+++ src/main/java/com/bankingserver/numblebank/common/SecurityConfig.java
@@ -1,25 +1,43 @@
 package com.bankingserver.numblebank.common;
 
+import com.bankingserver.numblebank.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private CorsConfig corsConfig;
+    @Autowired
+    private UserRepository userRepository;
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                        .requestMatchers("/sign-up").permitAll()
+                        .requestMatchers("/sign-up", "/login").permitAll()
                         .anyRequest().authenticated()
                 .and()
+                .addFilter(corsConfig.corsFilter())
+                .addFilterBefore(new JwtAuthenticationFilter(userRepository), UsernamePasswordAuthenticationFilter.class)
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
